e1f008f49144f0bdbbe9b4eb1a537550212af83b
Ganesh-Surya-Manimohan <surya.manimohan@gmail.com>
2025-03-12 10:58:39 +0530

    VPC test updated

--- outputs.tf
+++ outputs.tf
@@ -2,14 +2,14 @@
 # #     value = module.vpc.azs # module.<module-name>.<output>
 # # }
 
-# output "vpc_id" {
-#   value = module.vpc.vpc_id
-# }
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
 
-# output "public_subnet_list" {
-#   value = module.vpc.public_subnet_ids
-# }
+output "public_subnet_list" {
+  value = module.vpc.public_subnet_ids
+}
 
-# output "igw_id" {
-#   value = module.vpc.igw_id
-# }
+output "igw_id" {
+  value = module.vpc.igw_id
+}
